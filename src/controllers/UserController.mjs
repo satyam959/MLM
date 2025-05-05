82be286030b6eb05278afab5c2fb8149a123fe5b
satyam959 <satyam@scriza.in>
2025-05-05 17:43:02 +0530

    UserController

--- src/controllers/UserController.mjs
+++ src/controllers/UserController.mjs
@@ -363,9 +363,65 @@ async resendOTP(req, res) {
   
 
   // Update profile
+  // async updateProfile(req, res) {
+  //   const { userId } = req.user; 
+  //   const updateData = req.body; 
+  
+  //   try {
+  //     if (req.file) {
+  //       updateData.image = req.file.path; 
+  //     }
+  
+  //     const updatedUser = await UserRepository.updateUserByUserId(userId, updateData);
+  
+  //     if (!updatedUser) {
+  //       return res.status(404).json({ message: "User not found" });
+  //     }
+  
+  //     const {
+  //       fullName,
+  //       phone,
+  //       email,
+  //       companyName,
+  //       address,
+  //       city,
+  //       state,
+  //       country,
+  //       pinCode,
+  //       image,
+  //     } = updatedUser;
+  
+  //     return res.status(200).json({
+  //       statusCode: 200,
+  //       success: true,
+  //       message: "Profile updated successfully",
+  //       user: {
+  //         fullName,
+  //         phone,
+  //         email,
+  //         companyName,
+  //         address,
+  //         city,
+  //         state,
+  //         country,
+  //         pinCode,
+  //         image,
+  //       },
+  //     });
+  //   } catch (error) {
+  //     return res.status(500).json({
+  //       statusCode: 500,
+  //       success: false,
+  //       message: "Error updating profile",
+  //       error: error.message,
+  //     });
+  //   }
+  // }
+  
+
   async updateProfile(req, res) {
     const { userId } = req.user; 
-    const updateData = req.body; 
+    const updateData = { ...req.query };  
   
     try {
       if (req.file) {
@@ -375,14 +431,17 @@ async resendOTP(req, res) {
       const updatedUser = await UserRepository.updateUserByUserId(userId, updateData);
   
       if (!updatedUser) {
-        return res.status(404).json({ message: "User not found" });
+        return res.status(404).json({
+          statusCode: 404,
+          success: false,
+          message: "User not found"
+        });
       }
   
       const {
         fullName,
         phone,
         email,
-        companyName,
         address,
         city,
         state,
@@ -399,7 +458,6 @@ async resendOTP(req, res) {
           fullName,
           phone,
           email,
-          companyName,
           address,
           city,
           state,
@@ -421,12 +479,11 @@ async resendOTP(req, res) {
 
   /// Delete profile
 async deleteProfile(req, res) {
-  const { userId } = req.user; // ✅ token se userId
+  const { userId } = req.user; 
   
   try {
     const deletedUser = await UserRepository.deleteUserByUserId(userId);
     
-    // If the user is not found in the database
     if (!deletedUser) {
       return res.status(404).json({
         statusCode: 404,
@@ -434,15 +491,12 @@ async deleteProfile(req, res) {
         message: "User not found"
       });
     }
-    
-    // Successful deletion, return 200 status code with success details
-    return res.status(200).json({
+        return res.status(200).json({
       statusCode: 200,
       success: true,
       message: "Profile deleted successfully"
     });
   } catch (error) {
-    // If there's an error, return 500 status code
     return res.status(500).json({
       statusCode: 500,
       success: false,
