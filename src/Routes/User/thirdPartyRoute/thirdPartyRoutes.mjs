9a529fab567cfd259c2b3c23a166ab5f569632f2
anuragscriza <anurag@scriza.in>
2025-05-22 17:32:27 +0530

    pay bill code added

--- src/Routes/User/thirdPartyRoute/thirdPartyRoutes.mjs
+++ src/Routes/User/thirdPartyRoute/thirdPartyRoutes.mjs
@@ -1,14 +1,14 @@
 import express from "express";
 
 import thirdPartyService from "../../../thirdPartyAPI/ThirdPartyService.mjs";
-//import verifyToken from '../../middelware/authMiddleware.mjs'
+import verifyToken from '../../../middelware/authMiddleware.mjs'
 import multer from 'multer';
 
 const router = express.Router();
 const formDataWithMulter = multer();
 
-router.get('/allProviderList', thirdPartyService.getProvider);
-router.post('/providerValidation', thirdPartyService.providerValidation);
-router.post('/billVerification', formDataWithMulter.none(), thirdPartyService.billVerification)
-
+router.get('/allProviderList', verifyToken, thirdPartyService.getProvider);
+router.post('/providerValidation', verifyToken, thirdPartyService.providerValidation);
+router.post('/billVerification', verifyToken, formDataWithMulter.none(), thirdPartyService.billVerification)
+router.post('/payBill', verifyToken, formDataWithMulter.none(), thirdPartyService.payBill)
 export default router;
--- src/thirdPartyAPI/ThirdPartyService.mjs
+++ src/thirdPartyAPI/ThirdPartyService.mjs
@@ -5,33 +5,6 @@ dotenv.config(); // Load .env variables first
 const { API_URL, API_TOKEN } = process.env;
 
 class ThirdPartyService {
-    // static async getProvider(req, res) {
-    //     try {
-    //         const searchParams = req.query.search
-    //         console.log("searchParams --", searchParams);
-    //         const data = { api_token: API_TOKEN };
-    //         const response = await apiClient.postMethod(`${API_URL}application/v1/get-provider`, data);
-    //         console.log("response -- ", response);
-    //         if (response) {
-    //             return res.status(200).json({
-    //                 success: true,
-    //                 message: "Provider data fetched successfully",
-    //                 data: response
-    //             });
-    //         } else {
-    //             return res.status(400).json({
-    //                 success: false,
-    //                 message: "Provider list not available",
-    //                 data: []
-    //             });
-    //         }
-
-    //     } catch (error) {
-    //         console.error('Error Getting Provider Data:', error.response?.data || error.message);
-    //         throw new Error('Failed to get Provider Data');
-    //     }
-    // }
-
 
     static async getProvider(req, res) {
         try {
@@ -125,6 +98,39 @@ class ThirdPartyService {
     static async isNumeric(value) {
         return !isNaN(value) && !isNaN(parseFloat(value));
     }
+
+    static async payBill(req, res) {
+        try {
+            const userId = req.user.userId;
+            const rawBody = req.body;
+
+            // const parsedBody = {};
+            // for (const key in rawBody) {
+            //     const value = rawBody[key];
+            //     // Convert to number only if it's a valid number string
+            //     parsedBody[key] = value ? Number(value) : value;
+            // }
+
+            const postData = {
+                api_token: API_TOKEN,
+                type: 1,
+                client_id: userId,
+                ...rawBody
+            };
+            const response = await apiClient.postMethod(`${API_URL}telecom/v1/payment`, postData);
+            if (response) {
+                return res.status(200).json({
+                    success: true,
+                    message: "Bill processed successfully",
+                    data: response
+                });
+            }
+        } catch (error) {
+            console.log('Error in bill verification :', error);
+            throw new Error('Failed to verify bill');
+        }
+    }
+
 
 }
 
